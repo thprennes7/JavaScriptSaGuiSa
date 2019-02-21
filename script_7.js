08e5388708f861cf618b078b7d0fa33bef23decb
Salomé Marzin <salome.mrzn@gmail.com>
2019-02-21 20:29:29 +0100

    script_7.js done

--- script_7.js
+++ script_7.js
@@ -23,3 +23,60 @@ function rentedAll(booksNew){
 };
 
 console.log(rentedAll(booksNew));
+
+function mostRented(booksNew){
+  let numBookRent = []
+  booksNew.forEach(book => {
+    numBookRent.push(book['rented']);
+  });
+  let indexOfMax = numBookRent.indexOf(Math.max(...numBookRent));
+  console.log(booksNew[indexOfMax]);
+};
+
+mostRented(booksNew);
+
+function leastRented(booksNew){
+  let numBookRent = []
+  booksNew.forEach(book => {
+    numBookRent.push(book['rented']);
+  });
+  let indexOfMin = numBookRent.indexOf(Math.min(...numBookRent));
+  console.log(booksNew[indexOfMin]);
+};
+
+leastRented(booksNew);
+
+function findId(booksNew, id){
+  let zeBook;
+  booksNew.forEach(book => {
+    if (book['id'] === id) {
+      zeBook = book;
+    };
+  });
+  return zeBook;
+};
+
+console.log(findId(booksNew, 873495));
+
+function deleteBook(booksNew, id){
+  let curatedBooks = []
+  booksNew.forEach(book => {
+    if (book['id'] !== id){
+      curatedBooks.push(book);
+    };
+  });
+  return curatedBooks;
+};
+
+console.log(deleteBook(booksNew, 133712));
+
+
+function booksSorted(booksNew){
+  let titleBook = []
+  booksNew.forEach(book => {
+    titleBook.push(book['title']);
+  });
+  titleBook.sort();
+  return titleBook;
+};
+console.log(booksSorted(booksNew));
