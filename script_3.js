c292447a7bfb430d1bdb4f4820483584211a52e2
Montblanc159 <guillaume.zaugg@gmail.com>
2019-02-21 14:58:29 +0100

    Pyramid working

---
+++ script_3.js
@@ -0,0 +1,13 @@
+let numFloor = prompt("Bienvenue dans Pyramide builder ! Combien veux tu d'étages ?"), stair = "#", space = " ";
+
+
+for(i = numFloor; i > 0; i--){
+  let floor = "";
+  for(let j = i - 1; j > 0; j--){
+    floor += space;
+  };
+  for(j = numFloor - floor.length - 1; j >= 0; j--){
+    floor += stair;
+  };
+  console.log(floor);
+};
